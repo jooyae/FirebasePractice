9e52717e0bd7bb89bdf7295792bb5ca79080e20b
JooYae <jooyae3970@naver.com>
2021-05-25 15:06:18 +0900

    [feat] sendNotification

--- app/src/main/java/org/sopt/firebasepractice/MainActivity.kt
+++ app/src/main/java/org/sopt/firebasepractice/MainActivity.kt
@@ -5,15 +5,18 @@ import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceIdReceiver
 import com.google.firebase.messaging.FirebaseMessagingService
 import java.io.IOException
+import java.security.KeyPairGenerator.getInstance
+import java.security.KeyStore.getInstance
+import java.util.Calendar.getInstance
+import java.util.Currency.getInstance
 
 class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
 
 }
--- app/src/main/java/org/sopt/firebasepractice/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/sopt/firebasepractice/MyFirebaseMessagingService.kt
@@ -1,8 +1,15 @@
 package org.sopt.firebasepractice
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     private val TAG = "FirebaseService"
@@ -14,6 +21,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         editor.putString("token", token).apply()
         editor.commit()
         super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.notification != null){
+            sendNotification(remoteMessage.notification?.title, remoteMessage.notification!!.body!!)
+        }
+    }
+    @SuppressLint("ServiceCast")
+    private fun sendNotification(title: String?, body: String){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSound(defaultSound)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
 }
