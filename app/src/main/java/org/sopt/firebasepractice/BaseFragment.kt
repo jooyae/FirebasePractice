db1c874d6da7e6bec6be8c7a9afbaaafd70a9d9a
JooYae <jooyae3970@naver.com>
2021-06-01 14:31:44 +0900

    base model

--- app/src/main/java/org/sopt/firebasepractice/BaseFragment.kt
+++
@@ -1,4 +0,0 @@
-package org.sopt.firebasepractice
-
-class BaseFragment {
-}
---
+++ app/src/main/java/org/sopt/firebasepractice/base/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package org.sopt.firebasepractice.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+   
+
+}
