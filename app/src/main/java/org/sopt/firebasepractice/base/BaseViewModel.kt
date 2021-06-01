0462c9dbdb70765a1f11e4b84c0ea4983e1c541a
JooYae <jooyae3970@naver.com>
2021-06-01 14:35:58 +0900

    compositeDisposable viewModel

--- app/src/main/java/org/sopt/firebasepractice/base/BaseViewModel.kt
+++ app/src/main/java/org/sopt/firebasepractice/base/BaseViewModel.kt
@@ -1,8 +1,19 @@
 package org.sopt.firebasepractice.base
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 open class BaseViewModel : ViewModel() {
-   
+    private val compositeDisposable = CompositeDisposable()
+    
+    fun addDisposable(d:Disposable){
+        compositeDisposable.add(d)
+    }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
 
 }
