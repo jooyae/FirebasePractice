3ebb0d386143a25c25c47ab9eb8cf5e99e403a4a
JooYae <jooyae3970@naver.com>
2021-06-03 17:11:59 +0900

    [FEAT] Calendar

---
+++ app/src/main/java/org/sopt/firebasepractice/CalendarActivity.kt
@@ -0,0 +1,39 @@
+package org.sopt.firebasepractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import org.sopt.firebasepractice.base.BaseActivity
+import org.sopt.firebasepractice.base.BaseViewModel
+import org.sopt.firebasepractice.calendar.CalendarFragment
+import org.sopt.firebasepractice.databinding.ActivityCalendarBinding
+
+class CalendarActivity : BaseActivity<ActivityCalendarBinding, BaseViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_calendar
+    override val viewModel: BaseViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+        showCalendarFragment()
+    }
+
+    fun showCalendarFragment(){
+        val calendarFragment = CalendarFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.calendar_fragment, calendarFragment)
+        transaction.commit()
+    }
+
+    override fun initStartView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initDataBinding() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initAfterBinding() {
+        TODO("Not yet implemented")
+    }
+}
